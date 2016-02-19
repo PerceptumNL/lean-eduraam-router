fada223d2a59f037c3e2fc10730a5655dd979405
mslatour <sanderlatour@gmail.com>
2016-02-19 18:47:45 +0100

    Added dynamic port setting.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/http*', function (req, res) {
   var asset = decodeURIComponent(req.path.substring(1));
   request.get(asset).pipe(res);
 });
 
-app.listen(3000, function () {
-	  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+	  console.log('Node app is running on port', app.get('port'));
 });
